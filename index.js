2d82d880809faba452a08c526c9201c156d8ad72
Alex Fu <alex.fu914@gmail.com>
2016-03-28 14:07:44 -0400

    New ping route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var mainHandler = require('./handlers/main.js');
 var authHandler = require('./handlers/auth.js');
+var pingHandler = require('./handlers/ping.js');
 
 // Setup
 var app = express();
@@ -9,6 +10,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.post('/', mainHandler.post);
 app.get('/auth', authHandler.get);
+app.get('/ping', pingHandler.get);
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
